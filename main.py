4949968c6657d3dc0f8debb9cb006a28996dc8ef
yashtripathi20005-creator <yashtripathi20005@gmail.com>
2026-06-19 17:36:11 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+# ===============================
+# File: main.py
+# ===============================
+# Entry point for the Snake game
+
+from game import Game
+
+if __name__ == "__main__":
+    game = Game()
+    game.run()
